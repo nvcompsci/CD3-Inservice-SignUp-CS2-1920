6c412fc5c53b81be88514e3a85e9d6bc9156f4e4
John Word <jword@sylvaniaschools.local>
2019-09-09 14:15:07 -0400

    added createCards method

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 //create global variable
 let sessions
+
+function loadPage() {
 fetch("sessions.json")
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {console.log(json)
+                  sessions = json
+                  createCards()})
     .catch(err => console.error(err))
-
+//createCards()
+}
+function createCards() {
+let container = document.createElement('div')
+container.innerHTML = sessions.map(session =>
+`<h3>${session.Title}</h3>
+<p>${session['Description']}</p>`
+                                  ).join('')
+document.querySelector('article')
+    .append(container)
+}
