9b91f89806c101ccee09cef3b64a2b99665445a6
John Word <jword@sylvaniaschools.local>
2019-09-10 14:20:11 -0400

    implement addToCart

--- script.js
+++ script.js
@@ -15,11 +15,27 @@ let container = document.createElement('div')
 container.innerHTML = sessions.map(session =>
 `<div>
 <h3>${session.Title}</h3>
-<h3>Presenters</h3>
+<h3>Presenter${session['Presenter(s)'].split(' and ').length > 1 ? 's' : ''}: ${session['Presenter(s)']}</h3>
 <p>${session['Description']}</p>
-<button>Sign Up</button>
+<button onclick="addToCart('${session['Title']}')">Sign Up</button>
 </div>`
                                   ).join('')
 document.querySelector('article')
     .append(container)
 }
+
+function addToCart(sessionName) {
+    let item = document.createElement('li')
+    item.innerHTML = sessionName
+    
+    document.querySelector('ul#cart')
+        .append(item)
+}
+
+
+
+
+
+
+
+
