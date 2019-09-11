c9832d789418169e6e90fd5b9a3bc99827470713
John Word <jword@sylvaniaschools.local>
2019-09-11 12:36:22 -0400

    prevent adding session twice

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //create global variable
 let sessions
 let cartCount = 0
+let inCart = []
 
 function loadPage() {
 fetch("sessions.json")
@@ -28,20 +29,26 @@ document.querySelector('article')
 }
 
 function addToCart(sessionName) {
-    let item = document.createElement('li')
-    item.innerHTML = sessionName
-    
-    document.querySelector('ul#cart')
-        .append(item)
-    
-    cartCount++
-    let cartCountElem = document.querySelector('span#cartCount')
-    cartCountElem.textContent = cartCount
-    if (cartCount >= 3) {
-        cartCountElem.style.backgroundColor = "green"
+    if (!inCart.includes(sessionName)) {
+        inCart.push(sessionName)
+        let item = document.createElement('li')
+        item.innerHTML = sessionName
+
+        document.querySelector('ul#cart')
+            .append(item)
+
+        cartCount++
+        let cartCountElem = document.querySelector('span#cartCount')
+        cartCountElem.textContent = cartCount
+        if (cartCount >= 3) {
+            cartCountElem.style.backgroundColor = "green"
+        }
+        else if (cartCount > 0) {
+            cartCountElem.style.backgroundColor = "aqua"
+        }
     }
-    else if (cartCount > 0) {
-        cartCountElem.style.backgroundColor = "aqua"
+    else {
+        alert("You have already added this session to your cart.")
     }
 }
 
